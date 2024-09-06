9ddabc1b424458a487e33ccc90342ca1c907eef4
Ferryal Fajar <ferryal21@gmail.com>
2024-09-06 17:03:58 +0700

    Adjustment component (#6)
    * fix: adjust components without component libraries
    
    * fix: adjust components toast & update package
    
    * fix: adjust components toas & add page login

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { Toaster } from '@/components/ui/toaster'
 import { ThemeProvider } from '@/components/theme-provider'
 import router from '@/router'
-import ReactQuery from '@/lib/ReactQuery';
+import { ToastProvider } from '@/components/ui/toast'
+import ReactQuery from '@/lib/ReactQuery'
 import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ReactQuery>
-    <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
-      <RouterProvider router={router} />
-      <Toaster />
-    </ThemeProvider>
+      <ToastProvider>
+        <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </ToastProvider>
     </ReactQuery>
   </React.StrictMode>
 )
